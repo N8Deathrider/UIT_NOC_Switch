1f9923eeb016d59b4f14d3dcd8e2d81cbb570212
N8Deathrider <nath001c@gmail.com>
2024-05-14 14:28:42 -0600

    Re arranged the code

--- SwitchHandler/vlan.py
+++ SwitchHandler/vlan.py
@@ -55,26 +55,6 @@ class Vlan:
         :rtype: int
         """
         return self._vlan_id
-    
-    @property
-    def status(self) -> str:
-        """
-        Get the status of the VLAN.
-
-        :return: The status of the VLAN.
-        :rtype: str
-        """
-        return self._status
-    
-    @property
-    def interfaces(self) -> list[str]:
-        """
-        Get the interfaces assigned to the VLAN.
-
-        :return: The interfaces assigned to the VLAN.
-        :rtype: list[str]
-        """
-        return self._interfaces
 
     @property
     def name(self) -> str:
@@ -97,15 +77,34 @@ class Vlan:
         """
         if not isinstance(name, str):
             raise ValueError("Name must be a string.")
-        
-        if self.vlan_id in [1, 1002, 1003, 1004, 1005]:
-            raise ValueError("VLANs 1, 1002, 1003, 1004, and 1005 are reserved and cannot be modified.")
 
-        commands = [
-            f"vlan {self.vlan_id}",
-            f"name {name}"
-        ]
+        if self.vlan_id in [1, 1002, 1003, 1004, 1005]:
+            raise ValueError(
+                "VLANs 1, 1002, 1003, 1004, and 1005 are reserved and cannot be modified."
+            )
+
+        commands = [f"vlan {self.vlan_id}", f"name {name}"]
         self._connection.send_config_set(commands)
         self._connection.save_config()
         self._name = name
         log.info(f"VLAN {self.vlan_id} name set to {name}")
+
+    @property
+    def status(self) -> str:
+        """
+        Get the status of the VLAN.
+
+        :return: The status of the VLAN.
+        :rtype: str
+        """
+        return self._status
+
+    @property
+    def interfaces(self) -> list[str]:
+        """
+        Get the interfaces assigned to the VLAN.
+
+        :return: The interfaces assigned to the VLAN.
+        :rtype: list[str]
+        """
+        return self._interfaces
